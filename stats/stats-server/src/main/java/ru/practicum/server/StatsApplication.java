fefd418a4149203c5dd71836b61c52b0262b8787
Roman <madara-09@bk.ru>
2023-01-31 11:31:19 +0300

    refactor: разбил сервис статистики на 3 подмодуля

--- stats/stats-server/src/main/java/ru/practicum/server/StatsApplication.java
+++ stats/stats-server/src/main/java/ru/practicum/server/StatsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StatApplication {
+public class StatsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(StatApplication.class, args);
+        SpringApplication.run(StatsApplication.class, args);
     }
 
 }
